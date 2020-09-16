1a6677807bc4b18cc03d2d041c90bc34318d48c6
noelma <noel-mathieu@hotmail.fr>
2020-09-16 22:30:53 +0200

    #183 Bugfix target lien de menu.

--- menu.php
+++ menu.php
@@ -7,8 +7,8 @@
                 <span class="opener"><?php echo $link[ 'title_link' ]; ?></span>
                 <?php echo $link[ 'submenu' ]; ?>
             <?php else: ?>
-            <a href="<?php echo $link[ 'link' ]; ?>" target="<?php echo $link[ 'target_link' ]; ?>" <?php if ($link[ 'target_link' ] === '_blank'): ?> rel="noopener noreferrer" <?php endif; ?>>
-                <?php echo !empty($link['icon']) ? "<i class='{$link['icon']}' aria-hidden='true'></i> {$link[ 'title_link' ]}" : $link[ 'title_link' ]; ?>
+            <a href="<?php echo $link[ 'link' ]; ?>"<?php if ($link[ 'target_link' ]): ?> target="_blank" rel="noopener noreferrer" <?php endif; ?>>
+                <?php echo !empty($link['icon']) ? "<i class='{$link['icon']}' aria-hidden='true'></i> " : ''; ?><?php echo $link[ 'title_link' ]; ?>
             </a>
             <?php endif; ?>
 
